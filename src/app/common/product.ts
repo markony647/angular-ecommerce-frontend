3b741c618ac71dcdfaaccf6aea0b7220df94d796
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-07-02 15:26:39 +0300

    Add product service

---
+++ src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+	ski: string;
+	name: string;
+	description: string;
+	unitPrice: number;
+	imageurl: string;
+	active: boolean;
+	unitsInStock: number;
+	dateCreated: Date;
+	lastUpdated: Date;
+}
